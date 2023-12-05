7b5e54c5d6479f7123b985a0b6dd32d40800f398
ubyhzargam <ubyhzargam@gmail.com>
2023-12-05 22:33:11 +0530

    Update main.cpp

--- selection_sort/main.cpp
+++ selection_sort/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  selection_sort
-//
-//  Created by Uby H on 05/12/23.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+void SelectionSort(int A[],int n)
+{
+    int i,j,k,s;
+    for(i=0;i<n-1;i++)
+    {
+        k=j=i;
+        for(j=i;j<n;j++)
+        {
+            if(A[j]<A[k])
+                k=j;
+        }
+        s=A[k];
+        A[k]=A[i];
+        A[i]=s;
+    }
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void Display(int A[],int n)
+{
+    cout<<"The elements of the array are given by : "<<endl;
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+}
+
+int main()
+{
+    cout<<"Enter the number of elements to be entered in the Array :"<<endl;
+    int n;
+    int A[50];
+    cin>>n;
+    cout<<"Enter the elements of the array : "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>A[i];
+    SelectionSort(A,n);
+    Display(A,n);
     return 0;
 }
